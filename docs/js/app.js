9f24896a6bdf8caeb434611e87d272175e92b526
r-naganuma <r.naganuma@roseaupensant.jp>
2020-02-10 17:53:18 +0900

    no message

--- docs/js/app.js
+++
@@ -1,7 +0,0 @@
-export default {
-    data(){
-      return {
-        isActive01: false
-      }
-    }
-}
--- docs/js/notFound.5fb98f9f.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["notFound"],{"8cdb":function(n,t,e){"use strict";e.r(t);var o=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("h1",[n._v("404 Not found")])},u=[],c=e("2877"),l={},r=Object(c["a"])(l,o,u,!1,null,null,null);t["default"]=r.exports}}]);
-//# sourceMappingURL=notFound.5fb98f9f.js.map
